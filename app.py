477a1fb8ccc166f0684f3cb04b8424eb59535a11
DavidChCadena <davidchcadena@gmail.com>
2022-06-27 20:28:10 -0500

    Update app.py
    Add properties to view

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 import random
 import math
 import collections
-import time
+from datetime import datetime
+import threading 
 from tkinter import *
-
 
 maxSimulationTime = 1000
 total_num = 0
+init_time = datetime.now()
+data_row = 2
 
 
 class Node:
@@ -149,59 +151,108 @@ def csma_cd(N, A, R, L, D, S, is_persistent):
             successfuly_transmitted_packets += 1
             #print("Paquetes exitosamente transmitidos:",successfuly_transmitted_packets)
             min_node.pop_packet()
-        else:    # If a collision occurred
+        else:    # If a collision occurred            
             min_node.collision_occured(R)
+            colision_msg()
             #print("Cantidad de colisiones:",min_node.collisions)
-
+    
+    efficiency = successfuly_transmitted_packets/float(transmitted_packets)
+    throughput = round(
+        (L * successfuly_transmitted_packets) / float(curr_time + (L/R)) * pow(10, -6),4
+    ), " Mbps"
     print("Paquetes exitosamente transmitidos:",
           successfuly_transmitted_packets)
     print("Cantidad de colisiones:", min_node.collisions)
-    print("Effeciency", successfuly_transmitted_packets/float(transmitted_packets))
-    print("Throughput", (L * successfuly_transmitted_packets) /
-          float(curr_time + (L/R)) * pow(10, -6), "Mbps")
+    
+    print("Effeciency", efficiency)
+    print("Throughput", throughput)
+    add_data(N, "packtes", successfuly_transmitted_packets, min_node.collisions, efficiency, throughput)
     print("")
 
-#Funcion que muestra el reloj y lo actualiza
-def times():
-    current_time = "Current time: " + time.strftime("%H:%M:%S")
-    clock_label.config(text=current_time)
-    clock_label.after(200, times)
+#Alert collision
+def colision_msg():
+    popup = Tk()
+    popup.wm_title("!")
+    label =Label(popup, text="Se detectó una colisión")
+    label.pack(side="top", fill="x", pady=10)
+    B1 = Button(popup, text="Ok", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
 
+#Count time
+def get_simulation_time():
+    time_elapsed = (datetime.now()-init_time).total_seconds()
+    return seconds_to_format_time(time_elapsed)
+
+def seconds_to_format_time(seconds):
+    hours = int(seconds / 60 / 60)
+    seconds -= hours*60*60
+    minutes = int(seconds/60)
+    seconds -= minutes*60
+    seconds = int(seconds)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+def refresh_time():
+    text="Tiempo de simulación: " + get_simulation_time()
+    clock_label.config(text=text)
+    gui.after(200, refresh_time)
 
 gui = Tk()
 gui.geometry("1000x500")
 gui.title("CSMA-CD Protocol Simulator")
 
-clock_label = Label(gui, font=("times", 20, "bold"))
-clock_label.grid(row=0, column=1, pady=25, padx=25)
-times()
+clock_label = Label(gui, font=("times", 15, "bold"))
+clock_label.grid(row=0, column=1, pady=10, padx=10)
 
+refresh_time()
 
-host_label = Label(gui, font=("times", 15, "bold"))
-host_label.grid(row=2, column=0, pady=25, padx=25)
-host_label.config(text="host_text")
+host_label = Label(gui, font=("times", 10, "bold"))
+host_label.grid(row=2, column=0, pady=10, padx=10)
+host_label.config(text="Host")
+
 # Variable N linea 220
 
-packets_label = Label(gui, font=("times", 15, "bold"))
-packets_label.grid(row=2, column=1, pady=25, padx=25)
-packets_label.config(text="transmitted_packets")
+packets_label = Label(gui, font=("times", 10, "bold"))
+packets_label.grid(row=2, column=1, pady=10, padx=5)
+packets_label.config(text="Paquetes transmitidos")
+
 # Variable en la linea 159
 
-su_packets_label = Label(gui, font=("times", 15, "bold"))
-su_packets_label.grid(row=2, column=2, pady=25, padx=25)
-su_packets_label.config(text="successfuly_transmitted_packets")
+su_packets_label = Label(gui, font=("times", 10, "bold"))
+su_packets_label.grid(row=2, column=2, pady=10, padx=10)
+su_packets_label.config(text="Paquetes entregados")
 # Esta variable esta en la linea 157
 
-collisions_label = Label(gui, font=("times", 15, "bold"))
-collisions_label.grid(row=2, column=3, pady=25, padx=25)
-collisions_label.config(text="collisions")
+collisions_label = Label(gui, font=("times", 10, "bold"))
+collisions_label.grid(row=2, column=3, pady=10, padx=10)
+collisions_label.config(text="Colisiones")
 # Variable en la linea 158, revisar si se estan detectando bien esas colisiones
+
+efficience_label = Label(gui, font=("times", 10, "bold"))
+efficience_label.grid(row=2, column=4, pady=10, padx=10)
+efficience_label.config(text="Eficiencia")
+
+throughput = Label(gui, font=("times", 10, "bold"))
+throughput.grid(row=2, column=5, pady=10, padx=10)
+throughput.config(text="Ancho de banda")
+
+
+def add_data(host, packets, sucess_packets, collisions, efficiency, throughput): 
+    global data_row   
+    data_row = data_row + 1
+    print("Data_row ", data_row)
+
+    Label(gui, font=("times", 10, "bold"), text=host).grid(row=data_row, column=0)
+    Label(gui, font=("times", 10, "bold"), text=packets).grid(row=data_row, column=1)
+    Label(gui, font=("times", 10, "bold"), text=sucess_packets).grid(row=data_row, column=2)
+    Label(gui, font=("times", 10, "bold"), text=collisions).grid(row=data_row, column=3)
+    Label(gui, font=("times", 10, "bold"), text=efficiency).grid(row=data_row, column=4)
+    Label(gui, font=("times", 10, "bold"), text=throughput).grid(row=data_row, column=5)
+        
 
 # La idea seria ver como meter las varibles en la interfaz, organizar todo,
 # ademas de revisar que mas pidio la ingeniera que falte
 
-
-gui.mainloop() #Funcion que muestra la ventana
 
 # Run Algorithm
 # N = The number of nodes/computers connected to the LAN
@@ -217,13 +268,17 @@ S = (2/float(3)) * C
 
 # Show the efficiency and throughput of the LAN (in Mbps) (CSMA/CD Persistent)
 # for N in range(20, 101, 20):
-for N in range(1, 3):
-    for A in [7, 10, 20]:
-        R = 1 * pow(10, 6)
-        L = 1500
-        print("Persistent: ", "Nodes: ", N, "Avg Packet: ", A)
-        csma_cd(N, A, R, L, D, S, True)
-
+def exect_simulation():  
+    for N in range(1, 3):
+        for A in [7, 10, 20]:
+            R = 1 * pow(10, 6)
+            L = 1500
+            print("Ejecucion") 
+            print("Persistent: ", "Nodes: ", N, "Avg Packet: ", A)
+            csma_cd(N, A, R, L, D, S, True)
+thread = threading.Thread(target=exect_simulation)
+thread.start()
+gui.mainloop() #Funcion que muestra la ventana
 
 # Show the efficiency and throughput of the LAN (in Mbps) (CSMA/CD Non-persistent)
 """for N in range(20, 101, 20):
