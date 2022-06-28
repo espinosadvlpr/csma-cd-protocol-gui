2a7b3301aa0feddf4f6bf3c2f7068d40d43a192d
David Espinosa <espinosadavid47@gmail.com>
2022-06-27 23:43:05 -0500

    (feature): version entrega

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import random
 import math
 import collections
-import threading 
+import threading
 from datetime import datetime
 from pandas import DataFrame
 import matplotlib.pyplot as plt
@@ -163,17 +163,18 @@ def csma_cd(N, A, R, L, D, S, is_persistent):
             successfuly_transmitted_packets += 1
             #print("Paquetes exitosamente transmitidos:",successfuly_transmitted_packets)
             min_node.pop_packet()
-        else:    # If a collision occurred            
+        else:    # If a collision occurred
             min_node.collision_occured(R)
             #print("Cantidad de colisiones:",min_node.collisions)
-    
+
     efficiency = successfuly_transmitted_packets/float(transmitted_packets)
     throughput = round(
-        (L * successfuly_transmitted_packets) / float(curr_time + (L/R)) * pow(10, -6),4
+        (L * successfuly_transmitted_packets) /
+        float(curr_time + (L/R)) * pow(10, -6), 4
     ), " Mbps"
     print("Paquetes exitosamente transmitidos:",
           successfuly_transmitted_packets)
-    print("Cantidad de colisiones:", min_node.collisions)    
+    print("Cantidad de colisiones:", min_node.collisions)
     print("Effeciency", efficiency)
     print("Throughput", throughput)
 
@@ -184,15 +185,19 @@ def csma_cd(N, A, R, L, D, S, is_persistent):
     AVG_PACKET.append(A)
     COLISSIONS.append(min_node.collisions)
 
-    add_data(N, transmitted_packets, successfuly_transmitted_packets, min_node.collisions, efficiency, throughput)
+    add_data(transmitted_packets, successfuly_transmitted_packets,
+             min_node.collisions, efficiency, throughput)
     plots_button = Button(gui, text="Ver Gráficos", command=plot_data)
     plots_button.grid(row=0, column=2)
     print("")
 
-#Count time
+# Count time
+
+
 def get_simulation_time():
     time_elapsed = (datetime.now()-init_time).total_seconds()
     return seconds_to_format_time(time_elapsed)
+
 
 def seconds_to_format_time(seconds):
     hours = int(seconds / 60 / 60)
@@ -202,42 +207,48 @@ def seconds_to_format_time(seconds):
     seconds = int(seconds)
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
+
 def refresh_time():
-    text="Tiempo de simulación: " + get_simulation_time()
+    text = "Tiempo de simulación: " + get_simulation_time()
     clock_label.config(text=text)
     gui.after(200, refresh_time)
+
 
 def plot_data():
     win = Toplevel(gui)
     data1 = {'Ancho': THROUGHPUT, 'Host': HOST}
     df1 = DataFrame(data1, columns=['Ancho', 'Host'])
-    figure1 = plt.Figure(figsize=(5,4), dpi=50)
+    figure1 = plt.Figure(figsize=(5, 4), dpi=50)
     ax1 = figure1.add_subplot(111)
     line1 = FigureCanvasTkAgg(figure1, win)
     line1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-    df1 = df1[['Ancho','Host']].groupby('Ancho').sum()
-    df1.plot(kind='line', legend=True, ax=ax1, color='r',marker='o', fontsize=10)
+    df1 = df1[['Ancho', 'Host']].groupby('Ancho').sum()
+    df1.plot(kind='line', legend=True, ax=ax1,
+             color='r', marker='o', fontsize=10)
     ax1.set_title('Ancho de banda vs Numero de host')
 
     data2 = {'Colisiones': COLISSIONS, 'Host': HOST}
     df2 = DataFrame(data2, columns=['Colisiones', 'Host'])
-    figure2 = plt.Figure(figsize=(5,4), dpi=50)
+    figure2 = plt.Figure(figsize=(5, 4), dpi=50)
     ax2 = figure2.add_subplot(111)
     line2 = FigureCanvasTkAgg(figure2, win)
     line2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-    df2 = df2[['Colisiones','Host']].groupby('Colisiones').sum()
-    df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
+    df2 = df2[['Colisiones', 'Host']].groupby('Colisiones').sum()
+    df2.plot(kind='line', legend=True, ax=ax2,
+             color='r', marker='o', fontsize=10)
     ax2.set_title('Colisiones vs Numero de host')
 
     data3 = {'Eficiencia': EFFICIENCY, 'Host': HOST}
     df3 = DataFrame(data3, columns=['Eficiencia', 'Host'])
-    figure3 = plt.Figure(figsize=(5,4), dpi=50)
+    figure3 = plt.Figure(figsize=(5, 4), dpi=50)
     ax3 = figure3.add_subplot(111)
     line3 = FigureCanvasTkAgg(figure3, win)
     line3.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-    df3 = df3[['Eficiencia','Host']].groupby('Eficiencia').sum()
-    df3.plot(kind='line', legend=True, ax=ax3, color='r',marker='o', fontsize=10)
+    df3 = df3[['Eficiencia', 'Host']].groupby('Eficiencia').sum()
+    df3.plot(kind='line', legend=True, ax=ax3,
+             color='r', marker='o', fontsize=10)
     ax3.set_title('Eficiencia vs Numero de host')
+
 
 gui = Tk()
 gui.geometry("1000x500")
@@ -250,7 +261,7 @@ refresh_time()
 
 host_label = Label(gui, font=("times", 10, "bold"))
 host_label.grid(row=2, column=0, pady=10, padx=10)
-host_label.config(text="Host")
+host_label.config(text="Cantidad de host")
 
 # Variable N linea 220
 
@@ -279,16 +290,27 @@ throughput.grid(row=2, column=5, pady=10, padx=10)
 throughput.config(text="Ancho de banda")
 
 
-def add_data(host, packets, sucess_packets, collisions, efficiency, throughput): 
-    global data_row   
+def add_data(packets, success_packets, collisions, efficiency, throughput):
+    global data_row
+    #data_row = data_row + 1
+
+    Label(gui, font=("times", 10, "bold"),
+          text=packets).grid(row=data_row, column=1)
+    Label(gui, font=("times", 10, "bold"),
+          text=success_packets).grid(row=data_row, column=2)
+    Label(gui, font=("times", 10, "bold"),
+          text=collisions).grid(row=data_row, column=3)
+    Label(gui, font=("times", 10, "bold"),
+          text=efficiency).grid(row=data_row, column=4)
+    Label(gui, font=("times", 10, "bold"),
+          text=throughput).grid(row=data_row, column=5)
+
+
+def add_initial_data(host):
+    global data_row
     data_row = data_row + 1
-    
-    Label(gui, font=("times", 10, "bold"), text=host).grid(row=data_row, column=0)
-    Label(gui, font=("times", 10, "bold"), text=packets).grid(row=data_row, column=1)
-    Label(gui, font=("times", 10, "bold"), text=sucess_packets).grid(row=data_row, column=2)
-    Label(gui, font=("times", 10, "bold"), text=collisions).grid(row=data_row, column=3)
-    Label(gui, font=("times", 10, "bold"), text=efficiency).grid(row=data_row, column=4)
-    Label(gui, font=("times", 10, "bold"), text=throughput).grid(row=data_row, column=5)
+    Label(gui, font=("times", 10, "bold"),
+          text=host).grid(row=data_row, column=0)
 
 
 # Run Algorithm
@@ -305,18 +327,22 @@ S = (2/float(3)) * C
 
 # Show the efficiency and throughput of the LAN (in Mbps) (CSMA/CD Persistent)
 # for N in range(20, 101, 20):
-def exect_simulation():  
-    for N in range(1, 3):
+
+
+def exect_simulation():
+    for N in range(20, 101, 20):
         for A in [7, 10, 20]:
             R = 1 * pow(10, 6)
             L = 1500
-            print("Ejecucion") 
-            print("Persistent: ", "Nodes: ", N, "Avg Packet: ", A)            
+            print("Ejecucion")
+            print("Persistent: ", "Nodes: ", N, "Avg Packet: ", A)
+            add_initial_data(N)
             csma_cd(N, A, R, L, D, S, True)
+
 
 thread = threading.Thread(target=exect_simulation)
 thread.start()
-gui.mainloop() #Funcion que muestra la ventana
+gui.mainloop()  # Funcion que muestra la ventana
 
 # Show the efficiency and throughput of the LAN (in Mbps) (CSMA/CD Non-persistent)
 """for N in range(20, 101, 20):
